6aa13bf633095e2bf758117d63994222e731fefe
Lungu Victor <lungulll98@gmail.com>
2025-11-29 10:42:09 +0200

    Add POJO Coffe class

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,2 +1,27 @@
 public class Coffee {
+
+    private int temp;
+    private int conc;
+
+
+    public Coffee(int temp, int conc) {
+        this.temp = temp;
+        this.conc = conc;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public int getConc() {
+        return conc;
+    }
+
+    @Override
+    public String toString() {
+        return "[temperatura cafelei = " +
+                 temp +
+                " : concentratia = " + conc +
+                ']';
+    }
 }
