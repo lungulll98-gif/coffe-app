4adb7cd8855d34e9e5d62c7b9e2b1d7403a23d1f
Lungu Victor <lungulll98@gmail.com>
2025-11-29 11:01:41 +0200

    Add TemperatureException

--- src/TemperatureException.java
+++ src/TemperatureException.java
@@ -1,2 +1,17 @@
 public class TemperatureException {
+    // props
+
+    int t;
+
+    // constructors
+
+
+    public TemperatureException(int t) {
+        this.t = t;
+
+    }
+
+    public int getT() {
+        return t;
+    }
 }
