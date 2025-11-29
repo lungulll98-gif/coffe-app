65f85c6ee198cfbfbd19343c0eff8b45be4f8389
Lungu Victor <lungulll98@gmail.com>
2025-11-29 11:26:38 +0200

    Add psvm

--- src/CoffeeTest.java
+++ src/CoffeeTest.java
@@ -1,2 +1,25 @@
 public class CoffeeTest {
+    public static void main(String[] args) {
+
+        CoffeeMaker mk = new CoffeeMaker();
+        DrinkCoffee d  = new DrinkCoffee();
+
+        for (int i = 0; i < 5; i++) {
+            Coffee coffee = mk.makeCoffee();
+
+            try {
+                d.drinkCoffe(coffee);
+            } catch (TemperatureException e) {
+                System.out.println("Exceptie T: " + e.getMessage() + "\ntemperatura: "
+                + e.getT());
+            } catch (ConcentrationCoffe e) {
+                System.out.println("Exceptie T: " + e.getMessage() + "\nconcentratia: "
+                        + e.getC());
+            }
+            finally {
+                System.out.println("Aparatul de cafea s-a oprit");
+            }
+        }
+
+    }
 }
